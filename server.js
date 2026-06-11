d9b546d0a4ed781bd2b09c2a2af2e844705dee50
fafnyrdance <fafnyr19@gmail.com>
2026-06-11 22:53:11 +0300

    Update server.js

--- server.js
+++ server.js
@@ -2,28 +2,33 @@ const express = require('express');
 const app = express();
 const http = require('http').createServer(app);
 const io = require('socket.io')(http);
-const path = require('path');
 
 app.use(express.static(__dirname));
 
 const users = {}, servers = {};
 
 io.on('connection', (socket) => {
-    socket.on('register user', (data, cb) => {
-        users[socket.id] = { id: socket.id, ...data, friends: [], servers: [] };
+    socket.on('register user', (d, cb) => {
+        users[socket.id] = { id: socket.id, ...d, avatar: "", customStatus: "", statusType: "online", aboutMe: "", friends: [], servers: [] };
         cb({ success: true, user: users[socket.id] });
+        io.emit('update users', users);
+    });
+
+    socket.on('update profile', (d) => {
+        if(users[socket.id]) { Object.assign(users[socket.id], d); io.emit('update users', users); }
     });
 
     socket.on('create server', (name, cb) => {
         const id = Math.random().toString(36).substr(2, 6);
-        servers[id] = { id, name, channels: ["загальний"] };
+        servers[id] = { id, name, owner: socket.id, channels: ["загальний", "флуд"] };
         cb({ success: true, server: servers[id] });
     });
 
-    socket.on('chat message', (msg) => {
-        io.emit('chat message', { user: users[socket.id]?.username || "Гість", ...msg });
+    socket.on('chat message', (m) => {
+        io.emit('chat message', { user: users[socket.id]?.username, ...m });
     });
+
+    socket.on('call-user', (d) => io.to(d.toId).emit('incoming-call', d));
 });
 
-const PORT = process.env.PORT || 3000;
-http.listen(PORT, () => console.log(`Server ready on port ${PORT}`));
+http.listen(process.env.PORT || 3000);
